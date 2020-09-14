a99f7b5cdeaf220727f25f8955dd08861b5f599f
Pavel Antsypovich <pavel.antsypovich@leverx.com>
2020-09-14 10:41:14 +0300

    add info

--- Lab_1/forms.py
+++ Lab_1/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label='Search words:', max_length=100)
+    search = forms.CharField(label='Search words', max_length=100)
